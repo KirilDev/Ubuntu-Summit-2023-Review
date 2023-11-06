def118222a6792510b7fae923bdfa4c7d91b4e27
Kirill_Dev <126055944+KirilDev@users.noreply.github.com>
2023-11-06 17:59:43 +0200

    Create just_file.c

---
+++ just_file.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+  string some_text;
+  printf("Enter something what you want: ");
+  scanf("@s", &some_text);
+  printf("Here we go, "&some_text);
+
+  return 0;
+}
